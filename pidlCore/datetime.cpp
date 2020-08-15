2dedb7b1ed5aa3023bb5354cd3db5d7174b852bb
NAGY, Krisztián <ipariparipa@gmail.com>
2020-08-15 23:42:19 +0200

    implement std::chrono based converters

--- pidlCore/datetime.cpp
+++ pidlCore/datetime.cpp
@@ -16,6 +16,16 @@ namespace PIDL {
 		return ret.tm_year >= 0 && ret.tm_mon >= 0 && dt.millisecond == 0 && dt.kind != DateTime::UTC;
 	}
 
+    extern PIDL_CORE__FUNCTION bool fromDateTime(const DateTime & dt, std::chrono::system_clock::time_point & ret)
+    {
+        tm tmp;
+        if(!fromDateTime(dt, tmp))
+            return false;
+        ret = std::chrono::system_clock::time_point(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::seconds(mktime(&tmp))) + std::chrono::milliseconds(dt.millisecond));
+
+        return true;
+    }
+
 	extern PIDL_CORE__FUNCTION tm fromDateTime(const DateTime & dt)
 	{
 		tm ret;
@@ -25,16 +35,29 @@ namespace PIDL {
 
 	extern PIDL_CORE__FUNCTION bool toDateTime(const tm & t, DateTime & ret)
 	{
-		ret.year = t.tm_year + 1900;
-		ret.month = t.tm_mon + 1;
-		ret.day = t.tm_mday;
-		ret.hour = t.tm_hour;
-		ret.minute = t.tm_min;
-		ret.second = t.tm_sec;
+        ret.year = static_cast<short>(t.tm_year + 1900);
+        ret.month = static_cast<short>(t.tm_mon + 1);
+        ret.day = static_cast<short>(t.tm_mday);
+        ret.hour = static_cast<short>(t.tm_hour);
+        ret.minute = static_cast<short>(t.tm_min);
+        ret.second = static_cast<short>(t.tm_sec);
 		ret.millisecond = 0;
 		ret.kind = DateTime::Local;
 		return ret.month <= 12;
 	}
+
+    extern PIDL_CORE__FUNCTION bool toDateTime(std::chrono::system_clock::time_point t, DateTime & ret)
+    {
+        time_t tt = std::chrono::duration_cast<std::chrono::seconds>(t.time_since_epoch()).count();
+        auto tmp = localtime(&tt);
+
+        if(!toDateTime(*tmp, ret))
+            return false;
+
+        ret.millisecond = static_cast<short>(std::chrono::duration_cast<std::chrono::milliseconds>(t.time_since_epoch()).count() - tt * 1000);
+
+        return true;
+    }
 
 	extern PIDL_CORE__FUNCTION DateTime toDateTime(const tm & t)
 	{
--- pidlCore/include/pidlCore/datetime.h
+++ pidlCore/include/pidlCore/datetime.h
@@ -5,6 +5,7 @@
 #include "config.h"
 
 #include <time.h>
+#include <chrono>
 
 namespace PIDL {
 
@@ -27,9 +28,11 @@ namespace PIDL {
 	};
 
 	extern PIDL_CORE__FUNCTION bool fromDateTime(const DateTime & dt, tm & ret);
-	extern PIDL_CORE__FUNCTION tm fromDateTime(const DateTime & dt);
+    extern PIDL_CORE__FUNCTION bool fromDateTime(const DateTime & dt, std::chrono::system_clock::time_point & ret);
+    extern PIDL_CORE__FUNCTION tm fromDateTime(const DateTime & dt);
 	extern PIDL_CORE__FUNCTION bool toDateTime(const tm & t, DateTime & ret);
-	extern PIDL_CORE__FUNCTION DateTime toDateTime(const tm & t);
+    extern PIDL_CORE__FUNCTION bool toDateTime(std::chrono::system_clock::time_point t, DateTime & ret);
+    extern PIDL_CORE__FUNCTION DateTime toDateTime(const tm & t);
 
 }
 
